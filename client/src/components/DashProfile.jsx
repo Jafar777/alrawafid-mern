d67558db9bc65481b41bc35bbf3d5675810099ad
jafar <144897803+Jafar777@users.noreply.github.com>
2024-07-16 12:17:18 +0300

    user  image upload funct

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,15 +1,97 @@
-import { Button, TextInput } from 'flowbite-react'
-import React from 'react'
+import { Alert, Button, TextInput } from 'flowbite-react'
+import { useEffect, useRef,useState  } from 'react';
 import {useSelector} from 'react-redux'
+import {getStorage ,ref ,uploadBytesResumable , getDownloadURL } from 'firebase/storage'
+import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+
+
 export default function DashProfile() {
     const {currentUser} = useSelector(state => state.user)
+    const [imageFile,setImageFile] = useState(null);
+    const [imageFileUrl, setimageFileUrl] = useState(null);
+    const [imageFileUploadingProgress, setimageFileUploadingProgress] = useState(null);
+
+    const [imageFileUploadingError, setimageFileUploadingError] = useState(null);
+
+
+    const filePickerRef = useRef();
+    const handleImageChange = (e)=> {
+        
+        const file = e.target.files[0];
+        if(file){
+            setImageFile(file);
+            setimageFileUrl(URL.createObjectURL(file))
+
+        }
+
+    };
+
+    useEffect(() => {
+        if (imageFile) {
+            uploadImage();
+        }
+    }, [imageFile])
+
+    const uploadImage = async () => {
+        
+    setimageFileUploadingError(null)    
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name ;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile)
+    uploadTask.on(
+        'state_changed',
+        (snapshot)=> {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100 ;
+            setimageFileUploadingProgress(progress.toFixed(0));
+        },
+        (error) => {
+            setimageFileUploadingError("Could not upload image (file must be less than 2mb"); 
+            setimageFileUploadingProgress(null);
+            setImageFile(null);
+            setimageFileUrl(null);
+        },
+        () =>{
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                setImageFile(downloadURL)
+            })
+        }
+
+    )
+    }
+
+    
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>الحساب الشخصي</h1>
       <form className='flex flex-col gap-4'>
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
-        <img src={currentUser.profilePicture} className='rounded-full w-full h-full object-cover border-8 border-[lightgray]' />
+        <input type="file" accept="/image/*" onChange={handleImageChange} ref={filePickerRef} hidden/>
+        <div className=" relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={()=> filePickerRef.current.click()}>
+            {imageFileUploadingProgress && (
+                <CircularProgressbar value={imageFileUploadingProgress || 0} text={`${imageFileUploadingProgress}% `}
+                strokeWidth={5}
+                styles={{
+                    root : {
+                        width :'100%',
+                        height : '100%',
+                        position: "absolute",
+                        top : 0,
+                        left : 0,
+                },
+                path : {
+                    stroke: `rgba(62,152,199, ${imageFileUploadingProgress / 100 })`,
+                },
+            }}
+                />
+            )}
+        <img src={imageFileUrl || currentUser.profilePicture} className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${imageFileUploadingProgress && imageFileUploadingProgress < 100 && 'opacity-60'}`} />
         </div>
+        {imageFileUploadingError && (<Alert color='failure'>{imageFileUploadingError}</Alert>) } 
+        
         <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}/>
         <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}/>
         <TextInput type='password' id='password' placeholder='password' />
