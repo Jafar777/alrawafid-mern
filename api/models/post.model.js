2de87db0343be414c0c17c581524e05275d53a63
jafar <144897803+Jafar777@users.noreply.github.com>
2024-07-24 10:01:38 +0300

    backed fix

--- api/models/post.model.js
+++ api/models/post.model.js
@@ -1,34 +1,38 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const postSchema = new mongoose.Schema (
-    {
-        userId:{
-            type:String,
-            required:true,
-        },
-        content: {
-            type: String,
-            required : true,
-        },
-        title: {
-            type: String,
-            required : true, 
-            unique: true,
-        },
-        image: {
-            type: String,
-            default : 'https://img.freepik.com/premium-vector/speech-bubble-with-new-offer-text-flat-cartoon-trend-modern-logotype-graphic-design-isolated-white-background_545399-2134.jpg',
-        },
-        category: {
-            type:String,
-            default: 'uncategorized',
-        },
-        slug: {
-            type: String, 
-            required : true,
-            unique: true ,
-        },
-    },  {timestamps:true}
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        'https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png',
+    },
+    category: {
+      type: String,
+      default: 'uncategorized',
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
 );
+
 const Post = mongoose.model('Post', postSchema);
-export default Post ;
+
+export default Post;
