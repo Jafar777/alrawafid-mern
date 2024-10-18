885e6aa1744bed1498228ee18dc8d184b24156bc
jafar <144897803+Jafar777@users.noreply.github.com>
2024-10-18 15:37:15 +0300

    updatehomecards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -91,12 +91,12 @@ export default function Home() {
 
       </div>
       {/* top ends here  */}
-      <Footer.Divider/>
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl text-right mx-auto mt-0'>
+     <div className='view'>
+      <div className='blockers flex flex-col gap-6 p-28 px-3 max-w-6xl text-right mx-auto mt-10 custom-bg-color3'>
           <Link to={'/about'}>
-          <h1 className='md:text-4xl sm:text-base hover:underline font-bold custom-font-color1'>من نحن؟</h1>
+          <h1 className='md:text-4xl sm:text-base hover:underline font-bold custom-font-color1 mr-3'>من نحن؟</h1>
           </Link>
-          <p id='arabic' className='mt-6 mb-6 text-lg font-bold custom-font-color3 text-right'>
+          <p id='arabic' className='mt-6 mb-6 text-lg font-bold text-white text-right'>
           تأسست مجموعة الروافد العقارية عام 2008 (1429هـ) لتصبح أحد الرواد في مجال المقاولات العامة وإدارة وتنفيذ المشاريع الكبرى في المملكة العربية السعودية. منذ انطلاقتنا، تركنا بصمة مميزة عبر تنفيذ مشاريع ضخمة ومتنوعة، مما يعكس التزامنا الدائم بتطبيق سياسات مدروسة تقوم على أسس علمية دقيقة. بفضل خبراتنا الممتدة لأكثر من عقد، نجحنا في تحقيق توازن مثالي بين الجودة والكفاءة، مما يعزز مكانتنا كشريك موثوق في قطاع البناء والمقاولات.
           </p>
           <Link to={'/about'} className='flex'>
@@ -105,8 +105,8 @@ export default function Home() {
           
       </div>  
 
-      <Footer.Divider/>
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl text-right mx-auto mt-0'>
+    
+      <div className='blockers flex flex-col gap-6 p-28 px-3 max-w-6xl text-right mx-auto mt-10'>
           <Link to={'/services'}>
           <h1 className='md:text-4xl sm:text-base hover:underline font-bold custom-font-color1'>ما هي خدمات شركة مجموعة الروافد التجارية ؟</h1>
           </Link>
@@ -119,15 +119,15 @@ export default function Home() {
           
       </div>  
 
-      <Footer.Divider/>
+    
       
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl text-right mx-auto mt-0'>
+      <div className=' blockers flex flex-col gap-6 p-28 px-3 max-w-6xl text-right mx-auto mt-10 custom-bg-color3'>
           <Link to={'/projects'}>
-          <h1 className='md:text-4xl sm:text-base hover:underline font-bold custom-font-color1'>
+          <h1 className='md:text-4xl sm:text-base hover:underline font-bold custom-font-color1 mr-3'>
             : مشاريع نفذتها مجموعة روافد
           </h1>
           </Link>
-          <p id='arabic' className='mt-6 mb-6 text-lg font-bold custom-font-color3 text-right'>نفذت مجموعة الروافد العقارية مجموعة متنوعة من المشاريع الكبرى التي تغطي مجالات متعددة في قطاع العقارات والبناء. تشمل مشاريعنا السكنية والتجارية، باستخدام أحدث التقنيات والابتكارات لضمان الجودة والاستدامة. تميزنا بإدارة وتنفيذ مشاريع ضخمة بدقة واحترافية، مما يعزز من مكانتنا كشركة رائدة في السوق السعودي. نعمل على تحقيق أعلى مستويات الرضا لعملائنا من خلال تقديم حلول مبتكرة تلبي احتياجاتهم وتساهم في تطوير البنية التحتية بما يتماشى مع رؤية المملكة 2030..
+          <p id='arabic' className='mt-6 mb-6 text-lg font-bold text-right text-white'>نفذت مجموعة الروافد العقارية مجموعة متنوعة من المشاريع الكبرى التي تغطي مجالات متعددة في قطاع العقارات والبناء. تشمل مشاريعنا السكنية والتجارية، باستخدام أحدث التقنيات والابتكارات لضمان الجودة والاستدامة. تميزنا بإدارة وتنفيذ مشاريع ضخمة بدقة واحترافية، مما يعزز من مكانتنا كشركة رائدة في السوق السعودي. نعمل على تحقيق أعلى مستويات الرضا لعملائنا من خلال تقديم حلول مبتكرة تلبي احتياجاتهم وتساهم في تطوير البنية التحتية بما يتماشى مع رؤية المملكة 2030..
           </p>
           <Link to={'/projects'} className='flex'>
           <button id='rawafidborder' className='flex flex-row items-center justify-center gap-5 custom-font-color1 rounded-full font-bold border-2 w-80 h-14'>الانقال لصفحة المشاريع <GoArrowRight className='bold text-3xl'/></button>
@@ -135,8 +135,8 @@ export default function Home() {
           
       </div>  
 
-      <Footer.Divider/>
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl text-right mx-auto mt-0 '>
+     
+      <div className='blockers flex flex-col gap-6 p-28 px-3 max-w-6xl text-right mx-auto mt-10 '>
       <Link to={'/orders'}>
         <h1 className='md:text-4xl sm:text-base hover:underline font-bold custom-font-color1'>
            التسويق العقاري
@@ -145,7 +145,7 @@ export default function Home() {
         <p id='arabic' className='mt-6 mb-6 text-lg font-bold custom-font-color3 text-right'>
         تقدم مجموعة الروافد العقارية حلولًا متكاملة للتسويق العقاري من خلال عروض متنوعة تناسب مختلف الاحتياجات. نساعد عملاءنا على استكشاف أفضل الفرص الاستثمارية في السوق من خلال قاعدة بيانات غنية بالعقارات المتاحة. كما نتيح لعملائنا إمكانية إرسال طلباتهم المخصصة، حيث يقوم فريقنا المتخصص بالبحث وتقديم العروض التي تتناسب تمامًا مع متطلباتهم. هدفنا هو تسهيل عملية الشراء أو الاستثمار العقاري من خلال توفير حلول تسويقية مبتكرة تلبي احتياجات الزبائن وتضمن أعلى مستويات الرضا.
         </p>
-        <Link to={'/projects'} className='flex'>
+        <Link to={'/listingsearch'} className='flex'>
           <button id='rawafidborder' className='flex flex-row items-center justify-center gap-5 custom-font-color1 rounded-full font-bold border-2 w-80 h-14'>العروض العقارية المتوفرة <GoArrowRight className='bold text-3xl'/></button>
           </Link>
           <br />
@@ -153,10 +153,10 @@ export default function Home() {
           <button id='rawafidborder' className='flex flex-row items-center justify-center gap-5 custom-font-color1 rounded-full font-bold border-2 w-80 h-14'>راسلنا هنا لطلب عقار معين <FaRegHandPointUp className='bold text-3xl'/></button>
           </Link>
       </div>
-      <Footer.Divider/>
+      </div>
 
 
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto mt-10 '>
         <h1 className='font-bold text-3xl custom-font-color1 mx-auto z-50'> بعض عملائنا</h1> 
           <div className='flex gap-6 p-10 px-3 max-w-4xl mx-auto'>
             <img src={nahdi} alt="" className='lg:w-60 lg:h-36 md:w-60 md:h-36 w-16 h-16' /> 
@@ -165,7 +165,7 @@ export default function Home() {
             <img src={alam} alt="" className='lg:w-60 lg:h-36 md:w-36 md:h-36 w-16 h-16' />
           </div>
       </div>
-    <Footer.Divider/>
+
     <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl text-right mx-auto'>
       <h1 className='font-bold text-3xl custom-font-color1 mx-auto mb-5'> زيارة الفرع الرئيسي</h1> 
       <p className='text-center custom-font-color3 text-lg font-bold flex justify-center items-center gap-5'><CiLocationOn className='text-red-600 text-3xl' />جدة - طريق الملك عبدالعزيز الفرعي - جانب بنك ساب الاول - مبنى 23514</p> 
